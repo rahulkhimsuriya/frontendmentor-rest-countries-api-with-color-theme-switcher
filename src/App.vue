e9d01758aafea93c5a459ac0e00535c42f121fe3
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2020-10-17 12:47:14 +0530

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,76 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <header>
+      <NavBar />
+    </header>
+    <main class="container">
+      <router-view />
+    </main>
   </div>
 </template>
 
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    NavBar
+  }
+}
+</script>
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,600;0,800;1,300&display=swap');
+
+* {
+  margin: 0%;
+  padding: 0%;
+  box-sizing: border-box;
+}
+
+:root {
+  --text-color: hsl(200, 15%, 8%); /* Very Dark Blue (Light Mode Text)*/
+  --input-text: hsl(0, 0%, 52%); /* (Light Mode Input) */
+  --bg-color: hsl(0, 0%, 98%); /* Very Light Gray (Light Mode Background) */
+  --bg-primary: hsl(
+    0,
+    0%,
+    100%
+  ); /* White (Dark Mode Text & Light Mode Elements) */
+}
+
+/* - Dark Blue (Dark Mode Elements): hsl(209, 23%, 22%) */
+/* - Very Dark Blue (Dark Mode Background): hsl(207, 26%, 17%) */
+
+/* - White (Dark Mode Text & Light Mode Elements): hsl(0, 0%, 100%) */
+
+body {
+  background: var(--bg-color);
+}
+
+body.dark {
+  --text-color: hsl(0, 0%, 100%); /* Very Dark Blue (Light Mode Text)*/
+  --input-text: hsl(0, 0%, 52%); /* (Light Mode Input) */
+  --bg-color: hsl(207, 26%, 17%); /* Very Dark Blue (Dark Mode Background) */
+  --bg-primary: hsl(209, 23%, 22%); /* Dark Blue (Dark Mode Elements) */
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Nunito Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-size: 16px;
+  font-weight: 300;
 }
 
-#nav {
-  padding: 30px;
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+header {
+  background-color: var(--bg-primary);
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <nav class="container navbar">
+    <router-link class="navbar__brand" to="/">Where in the world?</router-link>
+
+    <button class="navbar__button">
+      <span
+        ><svg
+          class="navbar__button--icon"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
+      </span>
+      <span>Dark Mode</span>
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 0;
+}
+
+.navbar a {
+  font-weight: bold;
+  color: var(--text-color);
+  text-decoration: none;
+}
+
+.navbar__brand {
+  font-size: 1.2rem;
+}
+
+.navbar__button {
+  display: flex;
+  align-items: center;
+  border: 0;
+  background-color: transparent;
+  color: var(--text-color);
+  cursor: pointer;
+}
+
+.navbar__brand:hover,
+.navbar__button:hover {
+  opacity: 0.75;
+}
+
+.navbar__button:focus {
+  outline: none;
+}
+
+.navbar__button--icon {
+  height: 1rem;
+  width: 1rem;
+  margin-right: 0.5rem;
+}
+</style>
